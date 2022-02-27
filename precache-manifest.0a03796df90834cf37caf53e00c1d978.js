da404f903eaa2cd53930b1fae4dc06ad6f4fa093
LakminiTharushika <96492@fhss.sjp.ac.lk>
2022-02-27 20:45:17 +0530

    Updates

---
+++ precache-manifest.0a03796df90834cf37caf53e00c1d978.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592f72440aee138dcc5e365ad3a3fdd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8828f504a466cd2457e2",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "36f5940f7c7dd10670d3",
+    "url": "/static/css/main.fa942388.chunk.css"
+  },
+  {
+    "revision": "8828f504a466cd2457e2",
+    "url": "/static/js/2.68b2eb11.chunk.js"
+  },
+  {
+    "revision": "76c33606446526c6ec38bc107ec65a87",
+    "url": "/static/js/2.68b2eb11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36f5940f7c7dd10670d3",
+    "url": "/static/js/main.83089304.chunk.js"
+  },
+  {
+    "revision": "8ed38580b61b68c9d879",
+    "url": "/static/js/runtime-main.154958d3.js"
+  },
+  {
+    "revision": "35764feaa76b6539eba083806c7305af",
+    "url": "/static/media/Hello.35764fea.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/static/media/laptop1.87561258.jpg"
+  },
+  {
+    "revision": "64dc8acccfc6f3eefd092a462445ff8a",
+    "url": "/static/media/sam.64dc8acc.jpg"
+  }
+]);
